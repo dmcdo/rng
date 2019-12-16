5f857c1fc10090cca57934edff15777019fe0020
Dylan McDougall <dylanmcdougall@protonmail.com>
2019-12-16 10:53:17 -0500

    Preemtively count instaces of elements in nums when bolding repeats

--- js/tables.js
+++ js/tables.js
@@ -17,21 +17,20 @@ function HTMLRandNumList (length, min, max, boldRepeats = false, allowRepeats = 
     if (sort) sortNumber(nums);
     let HTML = '<table class="numList">';
         HTML += '<tr>'; // Numbers will occupy one inline row
+            if (boldRepeats) {
+                let instancesOf = countInstances(nums);
 
-            for (let i = 0; i < nums.length; i++) { // For each number
-
-                if (boldRepeats) { // Identify repeats
-                    let filteredNumsArray = nums.filter(num => num === nums[i]).length;
-                    if (filteredNumsArray > 1) // If more than one instance of the current number in array
-                        HTML += '<td class="listElem listElemRepeat">' + nums[i] + '</td>'; // Bold cell
+                nums.forEach(num => {
+                    if (instancesOf[num] > 1)
+                        HTML += '<td class="listElem listElemRepeat">' + num + '</td>';
                     else
-                        HTML += '<td class="listElem">' + nums[i] + '</td>'; // Normal cell
-                }
-
-                else { // Don't identify repeats
-                    HTML += '<td>' + nums[i] + '</td>'; // Normal cell
-                }
-
+                        HTML += '<td class="listElem">' + num + '</td>';
+                });
+            }
+            else {
+                nums.forEach(num => {
+                    HTML += '<td class="listElem">' + num + '</td>';
+                });
             }
         HTML += '</tr>';
     HTML += '</table>';
@@ -48,4 +47,17 @@ function pad(n, width, z) {
     z = z || '0';
     n = n + '';
     return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
+function countInstances(array) {
+    let instancesOf = {};
+
+    array.forEach(elem => {
+        if (!instancesOf[elem])
+            instancesOf[elem] = 1;
+        else
+            instancesOf[elem]++;
+    });
+
+    return instancesOf;
 }
